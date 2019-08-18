de87fa0077db3a34d0de8f298d91765cbbe9c6cc
Lay <rainlay@gmail.com>
2019-08-18 15:05:47 +0800

    declare variable and iterating it

---
+++ cards/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main()  {
+	// var card string = "Ace of Spades"
+	//cards := []string{newCard(),newCard()}
+	cards := []string{"Ace of Diamonds",newCard()}
+	cards = append(cards,"Six of Spades")
+
+	// like php foreach
+	// i => index
+	// card => current iterating cards value
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+func newCard() string{
+	return "Five of Diamonds"
+}
