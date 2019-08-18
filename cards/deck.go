447648a3fd4ad5cc2bbeb9bf682ff4b10571ea11
Lay <rainlay@gmail.com>
2019-08-18 21:19:49 +0800

    add type dec new func: newDeck()

--- cards/deck.go
+++ cards/deck.go
@@ -4,6 +4,21 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardsSuits := []string{"Spades", "Diamonds", "Hearts", "Clue"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardsSuits {
+		for _, value := range cardValues {
+			cards = append(cards, suit+" of "+value)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
--- cards/main.go
+++ cards/main.go
@@ -1,14 +1,7 @@
 package main
 
 func main()  {
-	// var card string = "Ace of Spades"
-	//cards := []string{newCard(),newCard()}
-	cards := deck{"Ace of Diamonds",newCard()}
-	cards = append(cards,"Six of Spades")
-
+	cards := newDeck()
 	cards.print()
 }
 
-func newCard() string{
-	return "Five of Diamonds"
-}
