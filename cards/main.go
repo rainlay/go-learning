2f59f1d8358818fe6d5d04b1e8f678126343058b
Lay <rainlay@gmail.com>
2019-08-18 15:25:25 +0800

    define print func at desc and call it

--- cards/main.go
+++ cards/main.go
@@ -1,19 +1,12 @@
 package main
 
-import "fmt"
-
 func main()  {
 	// var card string = "Ace of Spades"
 	//cards := []string{newCard(),newCard()}
 	cards := deck{"Ace of Diamonds",newCard()}
 	cards = append(cards,"Six of Spades")
 
-	// like php foreach
-	// i => index
-	// card => current iterating cards value
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 }
 
 func newCard() string{
