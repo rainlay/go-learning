c95eddf3c1ed5d803c651d50a6e123dc52a4bb2e
Lay <rainlay@gmail.com>
2019-08-25 20:24:15 +0800

    write tests for newDeck()

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -5,7 +5,18 @@ import "testing"
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
-	if len(d) != 2000 {
+	if len(d) != 16 {
 		t.Errorf("Expected deck length of 16, but got %v", len(d))
 	}
+
+	if d[0] != "Spades of Ace" {
+		t.Errorf("Expected first card of Spades of Ace, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Clue of Four" {
+		t.Errorf("Expected last card of Clue of Four, but got %v", d[len(d)-1])
+	}
 }
+
+
+
