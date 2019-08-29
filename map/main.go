5486d4a634572a57fa3436a376aaaf359e490ec9
RAIN <rainlay@gmail.com>
2019-08-29 11:30:55 +0800

    more about map

--- map/main.go
+++ map/main.go
@@ -3,10 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red" : "#ff0000",
-		"green" : "#4bf745",
-	}
+
+	// #1
+	//var colors map[string]string
+
+	// #2
+	colors := make(map[string]string)
+
+	// #3
+	//colors := map[string]string{
+	//	"red" : "#ff0000",
+	//	"green" : "#4bf745",
+	//}
+
+	colors["white"] = "#ffffff"
+
+	delete(colors, "white")
 
 	fmt.Println(colors)
 }
