bc2efdde67045162deb42361886d4a4d8a1c293b
Lay <rainlay@gmail.com>
2019-10-04 22:32:03 +0800

    go routines practice, channel block

---
+++ channels/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	// create channel
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	fmt.Println(<-c)
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+
+	if err != nil {
+		fmt.Println(link, "site might be down...")
+		c <- "well, site might be down I think"
+		return
+	}
+
+	fmt.Println(link, "site is up")
+	c <- "oh... site is running "
+}
